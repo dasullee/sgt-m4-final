882726ef3336f5ea03fac3ba320167cdc5d745f3
Nicole Lee <nicoledasullee@gmail.com>
2019-10-13 07:04:19 +0000

    made connection to pool

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const PORT =  process.env.PORT || 9000;
 
 const app = express();
+const db = require('./db')
 
-app.get('/api/test', (req, res) => {
+app.get('/api/test', async (req, res) => {
+    const [result] = await db.query('SELECT * FROM grades');
+
     res.send({
-        message: "Test route /api/test route working!"
+        message: "Test route /api/test route working!",
+        result
     })
 })
 
